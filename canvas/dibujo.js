f0b60bdc15b484a7da316ab4850e2835091f6c43
FernandoGimenez <phergz@gmail.com>
2022-07-14 05:55:43 -0300

    dibujo en canvas con ejercicio

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,5 +1,22 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var linea = 100;
+var color_dib = "#C1AFE3"
+var yi, xf;
+var color_lad = "#554D63"
+var interlineado = 300 / linea
+
+dibujarLinea(color_lad, 1, 1, 1, 299);
+dibujarLinea(color_lad, 1, 299, 299, 299);
+dibujarLinea(color_lad, 299, 299, 299, 1);
+dibujarLinea(color_lad, 299, 1, 1, 1);
+
+for(i=0; i<linea; i++)
+{
+yi = (i * interlineado);
+xf = 300 - interlineado * (i + 1);
+dibujarLinea(color_dib, 300, yi, xf, 300);
+}
 
 function dibujarLinea(color, x_i, y_i, x_f, y_f)
 {
@@ -10,5 +27,3 @@ function dibujarLinea(color, x_i, y_i, x_f, y_f)
     lienzo.stroke();
     lienzo.closePath();
 }
-
-dibujarLinea("green", 40, 10, 300, 250)
