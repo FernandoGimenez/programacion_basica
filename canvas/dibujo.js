bad8529cdc58ea2dd18bbea2c908fd09aff33ea6
FernandoGimenez <phergz@gmail.com>
2022-07-14 07:31:31 -0300

    formularios y eventos en html y JS

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,22 +1,16 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-var linea = 100;
-var color_dib = "#C1AFE3"
-var yi, xf;
-var color_lad = "#554D63"
-var interlineado = 300 / linea
+var ancho = d.width;
+var color_lad = "#554D63";
 
 dibujarLinea(color_lad, 1, 1, 1, 299);
 dibujarLinea(color_lad, 1, 299, 299, 299);
 dibujarLinea(color_lad, 299, 299, 299, 1);
 dibujarLinea(color_lad, 299, 1, 1, 1);
-
-for(i=0; i<linea; i++)
-{
-yi = (i * interlineado);
-xf = 300 - interlineado * (i + 1);
-dibujarLinea(color_dib, 300, yi, xf, 300);
-}
 
 function dibujarLinea(color, x_i, y_i, x_f, y_f)
 {
@@ -26,4 +20,28 @@ function dibujarLinea(color, x_i, y_i, x_f, y_f)
     lienzo.lineTo(x_f, y_f);
     lienzo.stroke();
     lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+    var linea = parseInt(texto.value);
+    var i = 0;
+    var color_dib = "#C1AFE3";
+    var yi, xf;
+    var interlineado = ancho / linea;
+
+    while(i < linea)
+{
+    yi = ancho - i * interlineado;
+    xf = (i + 1) * interlineado;
+    dibujarLinea(color_dib, 0, yi, xf, 0);
+    i = i + 1;
+}
+
+    for(i=0; i<linea; i++)
+    {
+        yi = (i * interlineado);
+        xf = ancho - interlineado * (i + 1);
+        dibujarLinea(color_dib, ancho, yi, xf, ancho);
+    }
 }
